317e1dcd885fd8862688424a617ee03f8a8e18da
Pankaj Bhandari <Pankaj.Bhandari@aristocrat.com>
2021-05-17 15:34:16 -0400

    added files for exception filter

---
+++ PankajAPI/Models/ApiError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PankajAPI.Models
+{
+    public class ApiError
+    {
+        public string Message { get; set; }
+
+        public string Details { get; set; }
+    }
+}
