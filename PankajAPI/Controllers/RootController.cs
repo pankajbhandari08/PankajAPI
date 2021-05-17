beb99ffee2ebe7c030f4b7e22c4ec9cf15afad37
Pankaj Bhandari <Pankaj.Bhandari@aristocrat.com>
2021-05-17 14:53:13 -0400

    Add project files.

---
+++ PankajAPI/Controllers/RootController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PankajAPI.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name =nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomsController.GetRooms), null)
+                }
+            };
+
+            return Ok(response);
+        }
+    }
+}
